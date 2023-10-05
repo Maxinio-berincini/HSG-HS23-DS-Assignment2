538b29896101ffcdc23c14313ec94fd89a559b05
xLeon-python <leon.muscat@outlook.de>
2023-10-05 13:07:53 +0200

    Task 2

--- src/main/java/com/example/searchengine/IndexFlipper.java
+++ src/main/java/com/example/searchengine/IndexFlipper.java
@@ -15,7 +15,29 @@ public class IndexFlipper {
             CSVReader csvReader = new CSVReader(new FileReader(indexFileName));
             List<String[]> csvLines = csvReader.readAll();
             Set<String[]> lines = new HashSet<>();
-            //TODO: define lines to contain the lines that should be printed to index_flipped.csv
+
+            for (String[] line : csvLines) {
+                String link = line[0];
+                for (int i = 1; i < line.length; i++) {
+                    Optional<String[]> foundArrayOpt = findArrayByFirstElement(lines, line[i]);
+
+                    // If found, extend array by one and add link
+                    if (foundArrayOpt.isPresent()) {
+                        String[] foundArray = foundArrayOpt.get();
+                        String[] newArray = Arrays.copyOf(foundArray, foundArray.length + 1);
+                        newArray[newArray.length - 1] = link;
+                        lines.remove(foundArray);  // remove the old array from the set
+                        lines.add(newArray);      // add the updated array to the set
+                    } else {
+                        // If not found, create new array and add link
+                        String[] newLine = new String[2];
+                        newLine[0] = line[i];
+                        newLine[1] = link;
+                        lines.add(newLine);
+                    }
+                }
+            }
+
             CSVWriter writer = new CSVWriter(new FileWriter(flippedIndexFileName),',', CSVWriter.NO_QUOTE_CHARACTER,' ',"\r\n");
             for (String[] line : lines) {
                 writer.writeNext(line);
@@ -25,6 +47,12 @@ public class IndexFlipper {
             e.printStackTrace();
         }
 
+    }
+
+    private static Optional<String[]> findArrayByFirstElement(Set<String[]> set, String element) {
+        return set.stream()
+                .filter(array -> array.length > 0 && array[0].equals(element))
+                .findFirst();
     }
 
 }
